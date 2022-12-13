4b473786ae84ad0f2152817f24002de00abc29e6
Fernando Ribeiro <fribeiro@ibm.com>
2022-12-12 21:35:10 -0300

    Initial commit.

---
+++ src/main/java/br/com/ribeiro/fernando/worklogbackend/WorklogBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.fernando.worklogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorklogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorklogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ribeiro/fernando/worklogbackend/WorklogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.fernando.worklogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorklogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
